5cca0efa7649c8e8008c5ffb6e6a342b08d0d06b
lucas-faget <lucasfaget98@gmail.com>
2025-11-23 19:36:39 +0100

    feat: add Directions constant to build exports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export type { IChessboard as Chessboard } from "./api/chessboard";
 
 export type * from "./coordinates/Position";
 export type * from "./coordinates/Direction";
+export { Directions } from "./coordinates/Directions";
 export type * from "./types/HistoryEntry";
 export type { PlayerDTO as Player } from "./dto/PlayerDTO";
 export type { PieceDTO as Piece } from "./dto/PieceDTO";

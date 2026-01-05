ac6f0d31da24f7245ccfb196f61e374d0747eac2
lucas-faget <lucasfaget98@gmail.com>
2026-01-05 22:03:05 +0100

    test: cover all Chess960 positions

--- tests/fen.test.ts
+++ tests/fen.test.ts
@@ -3,7 +3,7 @@ import { Fen } from "../src/fen/Fen";
 
 describe("Fen", () => {
     it("should parse FEN string rightly", () => {
-        const fen: Fen = Fen.fromFenString("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        const fen: Fen = Fen.fromString("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         expect(fen.position).toBe("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
         expect(fen.activePlayer).toBe("w");
         expect(fen.castlingRecord).toEqual({
---
+++ tests/fischerRandom.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { Chess960Rows } from "./data/Chess960Rows";
+import { FischerRandomChess } from "../src/chess/FischerRandomChess";
+
+describe("Fischer Random Chess", () => {
+    for (const [id, row] of Object.entries(Chess960Rows)) {
+        it(`should returns the starting FEN string for position ${id}`, () => {
+            const chess: FischerRandomChess = new FischerRandomChess(parseInt(id));
+            expect(chess.toFen()).toBe(`${row}/pppppppp/8/8/8/8/PPPPPPPP/${row.toUpperCase()} w KQkq - 0 1`);
+        });
+    }
+});
